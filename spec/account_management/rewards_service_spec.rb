c91213fc2fde4f14b3ff7e7f50d3fe21890cd07f
Ikenna N. Okpala <me@ikennaokpala.com>
2014-07-15 13:27:34 +0100

    Cosmetic changes

--- spec/account_management/rewards_service_spec.rb
+++ spec/account_management/rewards_service_spec.rb
@@ -2,50 +2,21 @@ require "spec_helper"
 
 describe AccountManagement::RewardsService do 
   describe "validates customer rewards" do
-    let(:account) { 1234 }
-    let(:subscriptions) { ["sports", "kids", "music"] }
-    let(:rewards_service) { AccountManagement::RewardsService.new(account, subscriptions) }
+    
+    context "when account is eligible" do
 
-    before do
-      allow(CustomerStatus::EligibilityService).to receive(:new)
-        .and_return(double "EligibilityService", 
-          output: "CUSTOMER_ELIGIBLE",
-          description: "Customer is eligible")  
-    end
-     
-    context "account is eligible" do
-      describe "for a mixture of channels" do
-        it "returns rewards for an array of channels" do
-          expect(rewards_service.available).to eql ["CHAMPIONS_LEAGUE_FINAL_TICKET", "KARAOKE_PRO_MICROPHONE"]
-        end
-      end
-
-      describe "only sports channel" do
-        let(:subscriptions) { ["sports"] }
-        it "returns rewards for sports channel" do
-          expect(rewards_service.available).to eql ["CHAMPIONS_LEAGUE_FINAL_TICKET"]
-        end
-      end
-
-      describe "no rewards channel" do
-        let(:subscriptions) { ["kids"] }
-        it "returns no rewards" do
-          expect(rewards_service.available).to eql []
-        end
-      end
     end
 
-    context "none eligible account" do
-      before do
-        allow(CustomerStatus::EligibilityService).to receive(:new)
-          .and_return(double "EligibilityService", 
-            output: "CUSTOMER_INELIGIBLE",
-            description: "Customer is not eligible")  
-      end
+    context "when a none eligible accountis supplied" do
+      
+    end
 
-      it "returns the ineligibility message" do
-        expect(rewards_service.available).to eql "Customer is not eligible"
-      end
+    context "when there is a technical failure" do
+      
+    end
+
+    context "when the supplied account number is invalid" do
+      
     end
   end  
 end
